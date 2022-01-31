05fd745e7fec8a9a5f24c03b15c8ea39a9748dad
Jaiden Chicote <jaiden_chicote@outlook.com>
2022-01-31 22:34:01 +1100

    feat: Added noisegeneratortest files

---
+++ Assets/Scripts/Tests/EditMode/Generator/Noise/NoiseGeneratorTests.cs
@@ -0,0 +1,39 @@
+using MiniProceduralGeneration.Generator;
+using NUnit.Framework;
+
+namespace MiniProceduralGeneration.Tests.EditMode.Generator.Noise
+{
+
+    public class NoiseGeneratorTests
+    {
+        #region - - - - Fields - - - -
+
+        #endregion
+
+        #region - - - - Constructors - - - -
+
+        public NoiseGeneratorTests()
+        {
+
+        }
+
+        #endregion Constructors
+
+        #region - - - - Test - - - -
+
+        [Test]
+        public void CreateStepOffsets_InvokeMethod_GeneratedOffsetValues()
+        {
+            // Arrange
+            NoiseGenerator noiseGnerator = new NoiseGenerator();
+
+            // Act
+
+            // Assert
+
+        }
+
+        #endregion
+    }
+
+}
